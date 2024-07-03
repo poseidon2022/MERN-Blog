90720a63fc6df1a697082756ac6bbed752d344c7
poseidon2022 <kidusm82@gmail.com>
2024-07-03 22:50:16 +0300

    route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Dashboard from "./Pages/Dashboard"
+import Signin from "./Pages/Signin"
+import Signup from "./Pages/Signup"
+import Projects from "./Pages/Projects"
 
 function App() {
   return (
-    <h1 className="text-3xl text-red-500">
-      MERN APPLICATION
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/About" element = {<About/>}/>
+        <Route path = "/sign-up" element = {<Signup />}/>
+        <Route path = "/sign-in" element = {<Signin />}/>
+        <Route path = "/dashboard" element = {<Dashboard />}/>
+        <Route path = "/projects" element = {<Projects />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
