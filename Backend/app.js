a1218fccc0d2f253aeaddc8e83aeaa47ca58ebdc
poseidon2022 <kidusm82@gmail.com>
2024-07-04 19:53:21 +0300

    Sign in backend done

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,21 @@
 const express = require("express")
 const mongooese = require("mongoose")
 const userRoutes = require("./routes/user.route")
+const authRoutes = require("./routes/auth.route")
 
 mongooese.connect("mongodb+srv://kidusm3l:1055967317@mernblog.0xlxsox.mongodb.net/").then(()=> {
     console.log("Database connected")
 })
 const app = express()
+
+app.use(express.json())
 const port = 3001
 app.listen(port, () => {
     console.log("App listening on port")
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
 
 
 
---
+++ Backend/controller/auth.controller.js
@@ -0,0 +1,26 @@
+const User = require("../Models/user.model.js")
+const bcrypt = require("bcrypt");
+
+const signup = async (req, res) => {
+    const {username, email,password} = req.body;
+    if (!email || !username || !password || username === "" || email === "" || password === "") {
+        return res.status(400).json({message: "All fields are required"})
+    }
+
+    const hashedpwd = bcrypt.hashSync(password, 10);
+    const newUser = new User({
+        username,
+        email,
+        password: hashedpwd
+    });
+
+    try {
+        await newUser.save();
+        res.json({message: "Signup successful"})
+    }
+    catch(error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+module.exports = signup
