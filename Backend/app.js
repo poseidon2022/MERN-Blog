0af9790c7d8d8ba616ccfc440dc2508be0344506
poseidon2022 <kidusm82@gmail.com>
2024-07-04 17:24:50 +0300

    DB setup

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,15 @@
 const express = require("express")
+const mongooese = require("mongoose")
+
+mongooese.connect("mongodb+srv://kidusm3l:1055967317@mernblog.0xlxsox.mongodb.net/").then(()=> {
+    console.log("Database connected")
+})
 const app = express()
 const port = 3001
 app.listen(port, () => {
     console.log("App listening on port")
 })
+
 
 
 
