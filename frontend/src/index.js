8b2f6d728dcdeb582ed8af14cde3c2d28eec5ff6
poseidon2022 <kidusm82@gmail.com>
2024-07-08 19:17:22 +0300

    persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {store} from './redux/store.js'
+import {store, persistor} from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store = {store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store = {store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
 
 // If you want to start measuring performance in your app, pass a function
